8d0af60eeca6fdeeea1e93f05c5e34b95a961121
Sacha <127155102+SachaCn@users.noreply.github.com>
2023-05-17 14:12:20 +0200

    Edit the script to be able to chain operations and add functions to clear the actual entry or clear a single element using backspace and an another functionnality to show the active operator

--- main.js
+++ main.js
@@ -3,59 +3,125 @@ const operatorButtons = document.querySelectorAll('.operator-signs');
 const equalButton = document.querySelector('#equal-btn');
 const clearButton = document.querySelector('#clear-btn');
 const clearEntryButton = document.querySelector('#clearEntry-btn');
+const backspaceButton = document.querySelector('#backspace-btn');
 const display = document.querySelector('#display');
 let firstValue = [];
 let secondValue = [];
 let operator = '';
+let solution = null;
 
-clearButton.addEventListener('click', () => clear());
+clearButton.addEventListener('click', () => clearAll());
+clearEntryButton.addEventListener('click', () => clearEntry());
+backspaceButton.addEventListener('click', () => backspace());
 equalButton.addEventListener('click',() => operate());
 numberButtons.forEach(btn => btn.addEventListener('click', () => getNumber(btn)));
-operatorButtons.forEach(btn => btn.addEventListener('click', () => getOperator(btn)));
+operatorButtons.forEach(btn => btn.addEventListener('click', () => {getOperator(btn); showActive(btn)}));
 
 function operate() {
-    let x = Number(firstValue.join(''));
-    let y = Number(secondValue.join(''));
-    if(operator === '+') return add(x, y);
-    if(operator === '-') return subtract(x, y);
-    if(operator === '×') return mutliply(x, y);
-    if(operator === '÷') return divide (x, y);
-};
-
-function add(a, b) {
-    return display.textContent = a + b;
-};
-
-function subtract(a, b) {
-    return display.textContent = a - b;
-};
-
-function mutliply(a, b) {
-    return display.textContent = a * b;
-};
-
-function divide(a, b) {
-    return display.textContent = a / b;
-};
-
-function clear() {
+    let a = Number(firstValue.join(''));
+    let b = Number(secondValue.join(''));
     firstValue = [];
     secondValue = [];
-    operator = '';
-    display.textContent = '0';
+    removeActive();
+    if(operator === '+') return add(a, b);
+    if(operator === '-') return subtract(a, b);
+    if(operator === '×') return mutliply(a, b);
+    if(operator === '÷') return divide (a, b);
 };
 
-function getNumber(arg) {
+function getNumber(pressed) {
     if(operator == '') {
-     firstValue.push(arg.textContent);
+     firstValue.push(pressed.textContent);
+     solution = null;
     display.textContent = firstValue.join('');   
     } else if (operator != '') {
-    secondValue.push(arg.textContent);
+    secondValue.push(pressed.textContent);
     display.textContent = secondValue.join('');
+    }
+};
+
+function getOperator(pressed) {    
+    if(solution == null && firstValue[0] == undefined) return;
+    if(solution != null) {
+        updateValue();
+    }
+    if(operator == '') {        
+        operator = pressed.textContent;                
+    } else if(operator != '') {
+        operate();
+        updateValue();
+        operator = pressed.textContent;        
     }    
 };
 
-function getOperator(arg) {
-    operator = arg.textContent;
-    display.textContent = arg.textContent;
+function add(a, b) {    
+    solution = a + b;
+    operator = '';
+    return display.textContent = solution;
+};
+
+function subtract(a, b) {    
+    solution = a - b;
+    operator = '';
+    return display.textContent = solution;
+};
+
+function mutliply(a, b) {    
+    solution = a * b;
+    operator = '';
+    return display.textContent = solution;
+};
+
+function divide(a, b) {    
+    solution = a / b;
+    operator = '';
+    return display.textContent = solution;
+};
+
+function updateValue() {
+    firstValue = [`${solution}`];
+    secondValue = [];
+    solution = null;
+};
+
+function clearAll() {
+    firstValue = [];
+    secondValue = [];
+    operator = '';
+    solution = null;
+    display.textContent = '0';
+    removeActive();
+};
+
+function clearEntry() {    
+    if(display.textContent == firstValue.join('')) {
+        display.textContent = '0';
+        firstValue = [];
+    } else if (display.textContent == secondValue.join('')) {
+        display.textContent = '0';
+        secondValue = [];
+    } else if(display.textContent == solution) {
+        clearAll();
+    }
+};
+
+function backspace() {
+    if(display.textContent == firstValue.join('')) {
+        firstValue.pop();
+        display.textContent = firstValue.join(''); 
+    } else if(display.textContent == secondValue.join('')) {
+        secondValue.pop();
+        display.textContent = secondValue.join('');
+    }   
+};
+
+function showActive(pressed) {
+    if(!pressed.classList.contains('active')) {
+        operatorButtons.forEach(e => e.classList.remove('active'));
+        pressed.classList.add('active');
+    }    
+};
+
+function removeActive() {
+    operatorButtons.forEach(e => e.classList.remove('active'));
 };
